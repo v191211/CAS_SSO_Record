a26c3bfe106c583ca3873d44149a0f47a38e7efc
V <v191211@gmail.com>
2020-02-22 05:35:27 +0800

    1. 修改cas服务器地址，Https改为http 2. 修改跳转登录页面地址，跳转到app1页面登录。

--- restful-api-sso/cas-app2/src/main/java/com/tingfeng/cas/config/CasConfig.java
+++ restful-api-sso/cas-app2/src/main/java/com/tingfeng/cas/config/CasConfig.java
@@ -13,22 +13,22 @@ public class CasConfig {
     /**
      * 当前应用程序的登陆界面
      */
-    public static String APP_LOGIN_PAGE = "http://app2.com:8282/water/login.html";
+    public static String APP_LOGIN_PAGE = "http://app1.com:8181/fire/login.html";
 
 
     /**
      * CAS服务器地址
      */
-    public static String CAS_SERVER_PATH = "https://cas.server.com:8443/cas";
+    public static String CAS_SERVER_PATH = "http://cas.server.com:8443/cas";
 
     /**
      * CAS登陆服务器地址
      */
-    public static String CAS_SERVER_LOGIN_PATH = "https://cas.server.com:8443/cas/login";
+    public static String CAS_SERVER_LOGIN_PATH = "http://cas.server.com:8443/cas/login";
 
     /**
      * CAS登出服务器地址
      */
-    public static String CAS_SERVER_LOGOUT_PATH = "https://cas.server.com:8443/cas/logout";
+    public static String CAS_SERVER_LOGOUT_PATH = "http://cas.server.com:8443/cas/logout";
 
 }
